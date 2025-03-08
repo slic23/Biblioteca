18579e74291f341fcbaebba7e2a63bb651336dfa
Issam Ramdani <socrates98@protonmail.com>
2025-03-08 14:39:06 +0100

    hecho

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,9 +3,33 @@ from django.contrib import admin
 # Register your models here.
 from .models import * 
 
-admin.site.register(Book)
+class ejemplares(admin.ModelAdmin):
+    list_filter = ("due_back","status")
+    list_display = ("book_id","status","due_back","id")
+    fieldsets = ((
+        None,{'fields':('book','imprint','id')}
+
+    ) , 
+    ('availability', {'fields':('status','due_back')}))
+
+
+class AdminAuthor(admin.ModelAdmin):
+    list_display = ("first_name","last_name","date_of_birth","date_of_death")
+    fields = ['first_name','last_name',('date_of_birth','date_of_death')]
+    
+    #se puede usar elemento exclude para excluir campos esto tendra mas efecto en los formularios
+
+class adminTabular(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+class bookadmin(admin.ModelAdmin):
+    list_display = ('title',)
+    inlines = [adminTabular]
+
+admin.site.register(Book,bookadmin)
 admin.site.register(Lenguage)
-admin.site.register(Author)
-admin.site.register(BookInstance)
+admin.site.register(Author,AdminAuthor)
+admin.site.register(BookInstance,ejemplares)
 admin.site.register(Genre)
+
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from .models import *
 
 # Create your views here.
+def index(request):
+
+    """
+    funcion principal del sitio catalog
+
+    """
+
+    num_libros = Book.objects.all().count()
+    numero_ejemplares = BookInstance.objects.all().count()
+    ejemplares_disponibles = BookInstance.objects.filter(status__exact='a').count()
+    numero_autores = Author.objects.all().count()
+    numero_generos = Genre.objects.all().count()
+    palabra_numeroVeces = Book.objects.filter(title__icontains= 'la').count()
+
+    contexto = {
+        'numero_libros':num_libros,
+        'numero_ejemplares': numero_ejemplares,
+        'ejemplares_disponibles': ejemplares_disponibles,
+        'numero_autores':numero_autores,
+        'numero_generos':numero_generos,
+        'palabrasEncontradas':palabra_numeroVeces
+
+    }
+
+
+    return render(request,'index.html',contexto)
+
+
